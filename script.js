483d4a19c4bf55aefa9b10f26356c9bb3d7e875e
LalelaniM <130096913+LalelaniM@users.noreply.github.com>
2026-06-23 12:46:58 +0200

    updated with the selector between daily and closing

--- script.js
+++ script.js
@@ -1,5 +1,75 @@
+ 
+ 
+ const selector = document.getElementById("pagechooser");
+        const dailyfigures = document.getElementById("dailyfigures");
+        const closingfigures = document.getElementById("closingfigures");
 
-async function calculateAndShow() {
+        selector.addEventListener("change", function () {
+            if (this.value === "daily") {
+                dailyfigures.classList.remove("hidden");
+                closingfigures.classList.add("hidden");
+            } else {
+                dailyfigures.classList.add("hidden");
+                closingfigures.classList.remove("hidden");
+            }
+        });
+
+
+async function calculateDaily() {
+
+  
+  const day = document.getElementById('day').value;
+  const time = document.getElementById('time').value;
+  const sales = document.getElementById('sales').value;
+  const target = document.getElementById('target').value;
+  const trans = document.getElementById('transactions').value;
+  const units = document.getElementById('units').value;
+  const traffic = document.getElementById('traffic').value;
+  const avt = document.getElementById('avt').value;
+  const upt = document.getElementById('upt').value;
+  const mtarget = 1400000; // Monthly target value
+
+  const messageDiv = document.getElementById('message');
+
+  let varFromTarget = "";
+  const targetCalc = target - sales;
+  if (targetCalc < 0) {
+    varFromTarget = `+R${Math.abs(targetCalc).toFixed(2)}`;
+  } else{
+      varFromTarget = `- R${Math.abs(targetCalc).toFixed(2)}`;
+    }
+
+  const PercentageToTarget = ((sales / target) * 100).toFixed(1);
+  const ConversionRate = ((trans / traffic) * 100).toFixed(1);
+  const mPercentageToTarget = ((mtdstarget / mtd) * 100).toFixed(1);
+
+  function formatNumber(num) {
+    return new Intl.NumberFormat('en-US').format(num).replace(/,/g, ' ');
+}
+
+
+  const msg = `#################<br><br>
+  Please see below our afternoon update.<br><br>
+  ${day}/06/2026<br><br>
+  *Figures* ${time}<br><br>
+  *Monthly Target*- R1 400 000<br>
+  *Actual*: R${formatNumber(sales)}<br>
+  *Target:* R${formatNumber(target)}<br>
+  *Var to Target:* ${(varFromTarget)}<br>
+  *Percentage To:* ${PercentageToTarget}%<br>
+  ################<br>
+  *Trans:* ${trans}<br>
+  *Units Sold:* ${units}<br>
+  *Foot Traffic:* ${traffic} EST<br>
+  *Conversion:* ${ConversionRate}%<br>
+  *AVT:* R${formatNumber(avt)}<br>
+  *UPT:* ${formatNumber(upt)}<br>
+  ################<br><br>`;
+  
+  document.getElementById('dailymessagetext').innerHTML = `${msg}`;
+}
+
+async function calculateClosing() {
 
   
   const day = document.getElementById('day').value;
@@ -62,12 +132,26 @@ async function calculateAndShow() {
   document.getElementById('messagetext').innerHTML = `${msg}`;
 }
 
-function copyResults() {
+
+
+function copyClosingResults() {
     const text = document.getElementById("messagetext").innerText;
 
     navigator.clipboard.writeText(text)
         .then(() => {
-            alert("Results copied to clipboard!");
+            alert("Message copied to clipboard!");
+        })
+        .catch(err => {
+            console.error("Copy failed", err);
+        });
+}
+
+function copyDialyResults() {
+    const text = document.getElementById("dailymessagetext").innerText;
+
+    navigator.clipboard.writeText(text)
+        .then(() => {
+            alert("Message copied to clipboard!");
         })
         .catch(err => {
             console.error("Copy failed", err);
